6cc9b39084983ed084fe6603dafe14b9c0ffb102
= <=>
2021-10-05 19:12:52 +0530

    Updated utils.py

--- acousvis/utils.py
+++ acousvis/utils.py
@@ -4,54 +4,6 @@ import plotly.graph_objects as go
 import os
 import librosa
 
-def plot_spectrogram(spectral_map, 
-                     time_values_present, 
-                     frequencies_values_present, 
-                     figname = "spectrogram", 
-                     save = False,
-                     title = None, 
-                     savepath = None,
-                     showcolorbar = True,
-                     log_ = False, 
-                     vmin = None, 
-                     vmax = None,
-                     width = 700,
-                     height = 500):
-    if log_:
-        spectral_map = librosa.core.power_to_db(np.abs(spectral_map)**2, ref=1, amin=1e-10, top_db=80.0)
-    else:
-        spectral_map = np.abs(spectral_map)
-    
-    if not vmin:
-        vmin = spectral_map.min()
-    if not vmax:
-        vmax = spectral_map.max()
-    
-    fig = go.Figure(data=go.Heatmap(z=spectral_map))
-    fig.show()
-    df = frequencies_values_present[1] - frequencies_values_present[0]
-    dt = time_values_present[1] - time_values_present[0]
-    im = axx.imshow(spectral_map, aspect = 'auto', origin = 'lower', extent = (time_values_present[0] - dt/2, time_values_present[-1] + dt/2, frequencies_values_present[0] - df/2, frequencies_values_present[-1] + df/2), vmin = vmin, vmax = vmax, cmap = cmap)
-    fig.update_layout(autosize = False, height = height, width = width, 
-                        margin=dict(l=5, r=5, t=5, b=5), xaxis_title='Frequency[Hz]',
-                        yaxis_title='Cumulative Activity')
-    
-    if not showcolorbar:
-        fig.update_traces(showscale = False)
-    
-    if title:
-        fig.update_layout(title_text = title)
-    
-    if save:
-        if savepath:
-            figname = os.path.join(savepath, figname + ".jpg")
-        else:
-            figname += ".jpg"
-        fig.write_image(figname, width = height + 50, height = width + 50)
-    else:
-        fig.show()
-    del fig
-
 def plot_3dplot(spectral_map, 
                 time, frequency, 
                 figname = "3dplot", 
